0c0851d18e9280a83ee0d3455290e580baeb9c9b
Lafie-rage <corentin.destrez62@gmail.com>
2022-01-28 17:05:55 +0100

    Initial commit

---
+++ angular-sw-heroes/README.md
@@ -0,0 +1,12 @@
+# AngularSwHeroes
+
+This project is a discovery of Angular during my front web lessons.
+You can find the repository here 
+
+## Tutorial source
+
+You can find the tutorial I followed [here](https://bisonfoutu.github.io/ng-sw-heroes).
+The tutorial is based on Angular official documentation. It was modified and fully translated in French.
+
+## Contributors
+- [Lafie-rage](https://github.com/Lafie-rage/)
---
+++ angular-sw-heroes/src/app/heroes/heroes.component.ts
@@ -0,0 +1,22 @@
+import {Component, OnInit} from '@angular/core';
+import {Hero} from "../models/hero.model";
+import {HeroService} from "../services/hero.service";
+
+@Component({
+  selector: 'sw-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  heroes: Hero[] = []
+
+  constructor(private heroService: HeroService) {
+  }
+
+  ngOnInit(): void {
+    this.heroService.getHeroes().subscribe(heroes =>
+      this.heroes = heroes
+    )
+  }
+}
